21a0749adf1e14536578bcc23fc09845b88220d2
masha <byseledka@gmail.com>
2017-04-04 20:06:28 +0300

    non elegant

--- js/table.js
+++ js/table.js
@@ -1,8 +1,8 @@
 
 var names = [
-{id: 4, username: 'admin', first_name: 'ad', last_name: 'min', password: '123123'},
-{id: 3, username: 'user_1', first_name: 'user', last_name: '11', password: '123123'},
-{id: 1, username: 'user_2', first_name: 'user', last_name: '22', password: '123123'}
+{id: 4, username: 'dadmin', first_name: 'ad', last_name: 'min', password: '123123'},
+{id: 3, username: 'suser_1', first_name: 'user', last_name: '11', password: '123123'},
+{id: 1, username: 'buser_2', first_name: 'user', last_name: '22', password: '123123'}
 ];
 
 $(document).ready(function() {
@@ -17,30 +17,39 @@ $(document).ready(function() {
 
   var sorting;
 
-  $('th').on("click", function(){
+  $("body").on("click", "th", function(event){
 
-    $('table').click(function(event){
-      var target = event.target.id;
-      console.log(target);
+      var target;
+      target = $(this).attr('id');
+      console.log($(this));
+      var td = document.getElementById(target);
+      console.log(td.classList);
 
-      sorting = names.sort(function(a,b){
-        console.log(target);
-        console.log(a);
-        console.log(a.target);
-        if (a.target > b.target) return 1;
-        if (b.target > a.target) return -1;
+      if (td.classList.contains("reverse")) {
+        sorting = names.sort(function(a,b){
+          if (a[target] > b[target]) return -1;
+          if (b[target] > a[target]) return 1;
+        });
+        var reverse = true;
+      } else {
+        sorting = names.sort(function(a,b){
+          if (a[target] > b[target]) return 1;
+          if (b[target] > a[target]) return -1;
     });
-
-      sorting;
-    })
-
+        var reverse = false;
+  }
 
     console.log(names);
 
     var table = document.getElementsByTagName("table")[0];
     table.parentNode.removeChild(table);
 
-    document.body.innerHTML = '<table class = "table"><tr><th>ID</th><th>Username</th><th>First Name</th><th>Last Name</th><th>Password</th></tr></table>'
+    if (reverse) {
+      document.body.innerHTML = '<table class = "table"><tr><th id="id" >ID</th><th id="username">Username</th><th id = "first_name">First Name</th><th id="last_name">Last Name</th><th id="password">Password</th></tr></table>';
+    } else {
+      document.body.innerHTML = '<table class = "table"><tr><th id="id" class="reverse">ID</th><th id="username" class="reverse">Username</th><th id = "first_name" class="reverse">First Name</th><th id="last_name" class="reverse">Last Name</th><th id="password" class="reverse">Password</th></tr></table>';
+    }
+
     for (var i in names){
       var sort_row = $('table').append('<tr><td>'+names[i].id+'</td><td>'+names[i].username+'</td><td>'+names[i].first_name+'</td><td>'+names[i].last_name+'</td><td>'+names[i].password+'</td></tr>');
     };
